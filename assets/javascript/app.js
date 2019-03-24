57c5e96fe830520415b1fb5acd8a70d4b27801ec
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-03-24 15:50:39 -0400

    update to db on connection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,34 +17,50 @@ var numberOfconnectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
 
 
-connectedRef.on("value", function(snapshot) {
+connectedRef.on("value", function (snapshot) {
 
   // If they are connected..
   if (snapshot.val()) {
 
     // remove on disconnect
     numberOfconnectionsRef.onDisconnect().remove();
-    
-    // Add user to the connections list.
-    numberOfconnectionsRef.push(true);
 
+    displayInputFormWithSubmit();
+
+    $("#submit-name").on("click", function (event) {
+      event.preventDefault();
+
+      var getInputName = $("#input-name").val().trim();
+
+      // Add user to the connections list.
+      numberOfconnectionsRef.update({
+        [getInputName]:
+        {
+          name: getInputName,
+          choices: ["rock", "paper", "scissors"]
+
+        }
+      });
+
+    });
+    
   }
 });
 
 
 // function to display form
-function displayInputFormWithSubmit(){
+function displayInputFormWithSubmit() {
   var form = $("<form>");
   var label = $("<label>");
   var inputText = $("<input>");
   var inputSubmitButton = $("<input>");
 
   // <label for="input-name">Enter Name: </label>
-  label.attr("for","input-name").text("Enter Name: ");
+  label.attr("for", "input-name").text("Enter Name: ");
   // <input type="text" id="input-name">
-  inputText.attr("type","text").attr("id","input-name");
+  inputText.attr("type", "text").attr("id", "input-name");
   // <input type="submit" id="submit-name">
-  inputSubmitButton.attr("type","submit").attr("id","submit-name");
+  inputSubmitButton.attr("type", "submit").attr("id", "submit-name");
 
   form.append(label).append(inputText).append(inputSubmitButton);
   $("#display-form").append(form);
